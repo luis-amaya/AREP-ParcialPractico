44d05df0b8f83d8f3e8bf10761bd3a5ad900a930
Luis <luis.amaya-o@mail.escuelaing.edu.co>
2021-09-08 14:02:54 -0500

    first Commit

---
+++ src/main/java/edu/escuelaing/co/arep/clima/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.co.arep.clima;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/edu/escuelaing/co/arep/clima/AppTest.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.co.arep.clima;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
